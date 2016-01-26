45264daa752b52244209ac7b16835fd6d1ca3d5d
fabien.rondeau@stadline.com <fabien.rondeau@stadline.com>
2016-01-26 09:37:22 +0100

    add comment

--- src/CentraleLille/DemoBundle/Tests/Service/DemoServiceTest.php
+++ src/CentraleLille/DemoBundle/Tests/Service/DemoServiceTest.php
@@ -12,12 +12,17 @@ class DefaultControllerTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetUser()
     {
+        //mock the anonymous token
         $anonymousMock = $this->getMockBuilder('Symfony\Component\Security\Core\Authentication\Token\AnonymousToken')
                 ->disableOriginalConstructor()->getMock();
         $anonymousMock->method('getUser')->will($this->returnValue(false));
+
+        //mock the token Storage
         $mock = $this->getMock('Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage');
         $mock->expects($this->once())->method('getToken')->will($this->returnValue($anonymousMock));
+
         $demoService = new DemoService($mock);
+        $demoService->getUser();
     }
 
     public function testSetRole()
