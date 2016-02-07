3b52e6121d21ade70cc472f517145218d55ace61
JamesHyot <james.hyot@gmail.com>
2016-02-07 23:36:00 +0100

    Created ProjectPageBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new CentraleLille\DemoBundle\CentraleLilleDemoBundle(),
             new CentraleLille\HomepageBundle\CentraleLilleHomepageBundle(),
+            new ProjectPageBundle\ProjectPageBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
