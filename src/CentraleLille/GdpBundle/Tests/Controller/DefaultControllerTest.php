d191d1b57fb26d0df95932f2466b83b99bd3e46b
Vladimir <vladimir@openio.io>
2016-02-08 10:43:35 +0100

    -- Fixed tests

--- src/CentraleLille/GdpBundle/Tests/Controller/DefaultControllerTest.php
+++ src/CentraleLille/GdpBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,9 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/gdp');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Hello GDP !', $client->getResponse()->getContent());
     }
 }
