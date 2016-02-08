3755c576ac0c883dfddaebec240fda0c35903562
Mat <mat@gmail.com>
2016-02-08 11:42:07 +0100

    Fix User entity

--- src/CentraleLille/CustomFosUserBundle/Entity/User.php
+++ src/CentraleLille/CustomFosUserBundle/Entity/User.php
@@ -2,7 +2,7 @@
 
 namespace CentraleLille\CustomFosUserBundle\Entity;
 
-use FOS\UserBundle\Model\User as BaseUser;
+use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
