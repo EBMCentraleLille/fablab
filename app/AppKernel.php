1352a98872462528a30a3fd934920b77fad6336c
mlechapt <martin.lechaptois@gmail.com>
2016-02-08 16:16:56 +0100

    1st front homepage version

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,11 +13,13 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new CentraleLille\DemoBundle\CentraleLilleDemoBundle(),
             new CentraleLille\HomepageBundle\CentraleLilleHomepageBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- src/CentraleLille/HomepageBundle/Controller/DefaultController.php
+++ src/CentraleLille/HomepageBundle/Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace CentraleLilleHomepageBundle\Controller;
+namespace CentraleLille\HomepageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -8,6 +8,73 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CentraleLilleHomepageBundle:Default:index.html.twig');
+    	$weeklyProject=[
+    		'projectName'=>'Projet De La semaine',
+    		'projectDescription'=>'Ceci est la description du projet de la semaine',
+    		'projectPicture'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg'
+    	];
+    	$news=[
+    		[
+    			'userName'=>'Martin Lechaptois',
+    			'projectName'=>'Project De Martin',
+    			'projectPic'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg',
+    			'newsType'=>'a commenté'
+    		],
+    		[
+    			'userName'=>'Martin Lechaptois',
+    			'projectName'=>'Project De Martin',
+    			'projectPic'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg',
+    			'newsType'=>'a commenté'
+    		],
+    		[
+    			'userName'=>'Martin Lechaptois',
+    			'projectName'=>'Project De Martin',
+    			'projectPic'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg',
+    			'newsType'=>'a commenté'
+    		],
+    		[
+    			'userName'=>'Martin Lechaptois',
+    			'projectName'=>'Project De Martin',
+    			'projectPic'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg',
+    			'newsType'=>'a commenté'
+    		],
+    		[
+    			'userName'=>'Martin Lechaptois',
+    			'projectName'=>'Project De Martin',
+    			'projectPic'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg',
+    			'newsType'=>'a commenté'
+    		]];
+    	$thematics=['Mécanique','Impression 3D','Arduino','Électronique','Drone','CAO'];
+    	$recentProjects=[
+    		[
+    			'userName'=>'Martin Lechaptois',
+    			'projectName'=>'Project De Martin',
+    			'projectPic'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg',
+    			'likes'=>19,
+    			'messages'=>3,
+    			'files'=>4
+    		],
+    		[
+    			'userName'=>'Martin Lechaptois',
+    			'projectName'=>'Project De Martin',
+    			'projectPic'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg',
+    			'likes'=>3,
+    			'messages'=>15,
+    			'files'=>2
+    		],
+    		[
+    			'userName'=>'Martin Lechaptois',
+    			'projectName'=>'Project De Martin',
+    			'projectPic'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg',
+    			'likes'=>5,
+    			'messages'=>9,
+    			'files'=>1
+    		]];
+        return $this->render('CentraleLilleHomepageBundle:Default:index.html.twig',[
+        	'weeklyProject' => $weeklyProject,
+        	'news' => $news,
+        	'thematics' => $thematics,
+        	'recentProjects' => $recentProjects,
+        ]);
     }
 }
