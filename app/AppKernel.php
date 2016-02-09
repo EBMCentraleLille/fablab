28c538eb1d126f8d0bf9195c1fba32da48cf02c2
pierloui <plbonnart@gmail.com>
2016-02-09 15:03:46 +0100

    Missing files

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new CentraleLille\DemoBundle\CentraleLilleDemoBundle(),
             new CentraleLille\ReservationBundle\ReservationBundle()
         );
 
