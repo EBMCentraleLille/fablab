08d52fb3891243cc808d6789158b3926e02fb4c7
march21 <skikar.elmehdi@gmail.com>
2016-02-22 19:07:35 +0100

    Ajout et affichage de machines : - /reservation/add_machine - /Admin

---
+++ src/CentraleLille/ReservationBundle/Controller/MachineController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace CentraleLille\ReservationBundle\Controller;
+
+use CentraleLille\ReservationBundle\Entity\Machine;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class MachineController extends Controller
+{
+    public function addMachineAction(Request $request)
+    {
+        $machine = new Machine();
+
+        $formBuilder = $this->get('form.factory')->createBuilder('form',$machine);
+
+        $formBuilder
+            ->add('machineName', 'text')
+            ->add('description', 'textarea')
+            ->add('Sauvegarder', 'submit')
+
+            ;
+
+        $form = $formBuilder->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($machine);
+            $em->flush();
+            $request->getSession()->getFlashBag()->add('notice',"La machine a bien été enregistrée");
+            return $this->redirect($this->generateUrl('centrale_lille_add_machine',array('prenom'=>'Michelle','nom'=>'Jean', 'form'=> $form->createView())));
+        }
+
+
+        return $this->render('ReservationBundle::add.html.twig',array('prenom'=>'Michelle','nom'=>'Jean','form' => $form->createView()));
+    }
+
+
+}
--- src/CentraleLille/ReservationBundle/Entity/Event.php
+++ src/CentraleLille/ReservationBundle/Entity/Event.php
@@ -21,13 +21,13 @@ class Event
      */
     private $id;
 
+
     /**
     protected $title;
 
     /**
-     * @var datetime
-     *
-     * @ORM\Column(name="creationDateTime", type="datetime")
+     * @var datetime $creationDateTime
+     * @ORM\Column(type="datetime")
      */
     protected $creationDateTime;
 
@@ -45,27 +45,48 @@ class Event
      */
     protected $endDateTime;
 
+
     /**
-     * @OneToOne()
+     * @var teamMember
+     *
+     * @ORM\Column(name="teamMember",type="string")
      */
     protected $teamMember;
 
     /**
      * @var
      *
-     * @ORM\Column(name="team", type="")
+     * @ORM\Column(name="project", type="object")
      */
-    protected $team;
+    protected $project;
 
     /**
      * @var
      *
-     * @ORM\Column(name="machine", type="machine")
+     * @ORM\OneToOne(targetEntity="Machine", cascade={"persist"})
      */
     protected $machine;
 
-
-
+    /**
+     * Event constructor.
+     * @param int $id
+     * @param $creationDateTime
+     * @param datetime $startDateTime
+     * @param datetime $endDateTime
+     * @param $teamMember
+     * @param $team
+     * @param $machine
+     */
+    public function __construct($id, datetime $startDateTime, datetime $endDateTime, $teamMember, $team, $machine)
+    {
+        $this->id = $id;
+        $this->creationDateTime = date("d-m-Y H:i:s");
+        $this->startDateTime = $startDateTime;
+        $this->endDateTime = $endDateTime;
+        $this->teamMember = $teamMember;
+        $this->team = $team;
+        $this->machine = $machine;
+    }
 
     /**
      * Get id
@@ -76,5 +97,103 @@ class Event
     {
         return $this->id;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getMachine()
+    {
+        return $this->machine;
+    }
+
+    /**
+     * @param mixed $machine
+     */
+    public function setMachine($machine)
+    {
+        $this->machine = $machine;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTeamMember()
+    {
+        return $this->teamMember;
+    }
+
+    /**
+     * @param mixed $teamMember
+     */
+    public function setTeamMember($teamMember)
+    {
+        $this->teamMember = $teamMember;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTeam()
+    {
+        return $this->team;
+    }
+
+    /**
+     * @param mixed $team
+     */
+    public function setTeam($team)
+    {
+        $this->team = $team;
+    }
+
+    /**
+     * @return datetime
+     */
+    public function getEndDateTime()
+    {
+        return $this->endDateTime;
+    }
+
+    /**
+     * @param datetime $endDateTime
+     */
+    public function setEndDateTime($endDateTime)
+    {
+        $this->endDateTime = $endDateTime;
+    }
+
+    /**
+     * @return datetime
+     */
+    public function getStartDateTime()
+    {
+        return $this->startDateTime;
+    }
+
+    /**
+     * @param datetime $startDateTime
+     */
+    public function setStartDateTime($startDateTime)
+    {
+        $this->startDateTime = $startDateTime;
+    }
+
+    /**
+     * @return datetime
+     */
+    public function getCreationDateTime()
+    {
+        return $this->creationDateTime;
+    }
+
+    /**
+     * @param datetime $creationDateTime
+     */
+    public function setCreationDateTime($creationDateTime)
+    {
+        $this->creationDateTime = $creationDateTime;
+    }
+
+
 }
 
