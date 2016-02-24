4ce8c57e7e4d2aa2431895c40cd5aa366ba41f8f
mlechapt <martin.lechaptois@gmail.com>
2016-02-24 10:44:24 +0100

    layout news-feed

--- src/CentraleLille/NewsFeedBundle/Controller/DefaultController.php
+++ src/CentraleLille/NewsFeedBundle/Controller/DefaultController.php
@@ -8,6 +8,59 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CentraleLilleNewsFeedBundle:newsFeed.html.twig');
+    	$recentProjects=[
+    		[
+    			'userName'=>'Martin Lechaptois',
+    			'projectName'=>'Project De Martin',
+    			'projectPic'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg',
+    			'likes'=>19,
+    			'messages'=>3,
+    			'files'=>4,
+                'date'=>"25/10/2011"
+    		],
+            [
+                'userName'=>'Martin Lechaptois',
+                'projectName'=>'Project De Martin',
+                'projectPic'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg',
+                'likes'=>19,
+                'messages'=>3,
+                'files'=>4,
+                'date'=>"25/10/2011"
+            ],
+            [
+                'userName'=>'Martin Lechaptois',
+                'projectName'=>'Project De Martin',
+                'projectPic'=>'http://thingiverse-production-new.s3.amazonaws.com/renders/71/73/1f/f0/10/1c60646068ae96e9d944ead31ad3c6ec_preview_featured.jpg',
+                'likes'=>19,
+                'messages'=>3,
+                'files'=>4,
+                'date'=>"25/10/2011"
+            ]];
+        $suggestions=[
+            [
+                'type'=>1,
+                'name'=>'Project De Martin'
+            ],
+            [
+                'type'=>2,
+                'name'=>'Catégorie Charles'
+            ],
+            [
+                'type'=>1,
+                'name'=>'Project De James'
+            ],
+            [
+                'type'=>2,
+                'name'=>'Catégorie Roger'
+            ],
+            [
+                'type'=>1,
+                'name'=>'Project De Martin'
+            ]
+            ]; 
+        return $this->render('CentraleLilleNewsFeedBundle:newsFeed.html.twig',[
+        	'recentProjects' => $recentProjects,
+            'suggestions' => $suggestions
+        ]);
     }
 }
