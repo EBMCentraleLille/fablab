9d2bcc3c80f5ba0a2d31461d07b400964f8c6dc9
YassineKhlayfia <yassine.khlayfia@gmail.com>
2016-02-29 22:40:17 +0100

    date and contstructor added to task

--- src/CentraleLille/GdpBundle/Entity/Task.php
+++ src/CentraleLille/GdpBundle/Entity/Task.php
@@ -49,6 +49,16 @@ class Task
      */
     private $author;
 
+    /**
+     * @ORM\Column(name="date", type="date")
+     */
+    private $date;
+
+
+    public function __construct()
+    {
+        $this->date = new \DateTime();
+    }
 
     /**
      * Get id
@@ -155,5 +165,28 @@ class Task
     {
         return $this->author;
     }
-}
 
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Task
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
