6e7bba184ee895a5e2f6e0e8e71867bc5372214d
YassineKhlayfia <yassine.khlayfia@gmail.com>
2016-03-01 09:24:57 +0100

    TaskController addaction updated

--- src/CentraleLille/GdpBundle/Controller/TaskController.php
+++ src/CentraleLille/GdpBundle/Controller/TaskController.php
@@ -2,8 +2,11 @@
 
 namespace CentraleLille\GdpBundle\Controller;
 
+use CentraleLille\GdpBundle\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use CentraleLille\GdpBundle\Form\TaskType;
 
 class TaskController extends Controller
 {
@@ -12,6 +15,28 @@ class TaskController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('CentraleLilleGdpBundle:Tasks:tasks.html.twig');
+        return $this->render('CentraleLilleGdpBundle:Task:index.html.twig');
+    }
+
+
+    public function addAction(Request $request){
+
+        $task = new Task();
+        $form = $this->get('form.factory')->create(new TaskType(), $task);
+
+        if ($form->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Tâche bien enregistrée.');
+
+            return $this->redirect($this->generateUrl('gdp_personal_page'));
+
+        }
+        return $this->render('CentraleLilleGdpBundle:Task:add.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
