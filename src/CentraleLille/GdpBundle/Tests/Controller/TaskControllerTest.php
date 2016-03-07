1bdf2c7ead50782224b6439dd0cf4eaee5502308
matthieu <ml.94230@gmail.com>
2016-03-07 13:29:08 +0100

    Fix test

--- src/CentraleLille/GdpBundle/Tests/Controller/TaskControllerTest.php
+++ src/CentraleLille/GdpBundle/Tests/Controller/TaskControllerTest.php
@@ -4,13 +4,29 @@ namespace CentraleLille\GdpBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * DefaultControllerTest Class Doc Comment
+ *
+ * @category DefaultControllerTest
+ * @package  DefaultControllerTest
+ * @author   Display Name <ml.94230@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/EBMCentraleLille/fablab
+ */
 class TaskControllerTest extends WebTestCase
 {
+    /**
+     * Test Index
+     *
+     * Description function
+     *
+     * @return something
+     */
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/gdp/tasks');
+        $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Task list', $client->getResponse()->getContent());
