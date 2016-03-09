e2aa3f9fe00a8fe86020b29e9df9357fc8d6c4d8
Maxime Herbaut <maximehrbt@gmail.com>
2016-03-09 13:41:44 +0100

    ready to merge

--- src/CentraleLille/SearchBundle/Controller/SearchStringController.php
+++ src/CentraleLille/SearchBundle/Controller/SearchStringController.php
@@ -53,46 +53,44 @@ class SearchStringController extends Controller
         if (is_null($search->getStringSearch())) {
             $result_machine = [];
             $result_user = [];
-        } else
-
-         {
+        } else {
 
         //Recherche User
-        $typeUser = $this->get('fos_elastica.index.fablab.User');
-        $query_part_user = new \Elastica\Query\BoolQuery();
+            $typeUser = $this->get('fos_elastica.index.fablab.User');
+            $query_part_user = new \Elastica\Query\BoolQuery();
 
-        $fieldQuery = new \Elastica\Query\Match();
-        $fieldQuery2 = new \Elastica\Query\Match();
-        $fieldQuery->setFieldQuery('email', $search->getStringSearch());
-        $fieldQuery->setFieldQuery('email', $search->getStringSearch());
-        $fieldQuery->setFieldFuzziness('email', 0.7);
-        $query_part_user->addShould($fieldQuery);
-        $fieldQuery2->setFieldQuery('username', $search->getStringSearch());
-        $fieldQuery2->setFieldQuery('username', $search->getStringSearch());
-        $fieldQuery2->setFieldFuzziness('username', 0.7);
-        $query_part_user->addShould($fieldQuery2);
-        $filters = new \Elastica\Filter\Bool();
-        $query_user = new \Elastica\Query\Filtered($query_part_user, $filters);
-        $result_user = $typeUser->search($query_user);
+            $fieldQuery = new \Elastica\Query\Match();
+            $fieldQuery2 = new \Elastica\Query\Match();
+            $fieldQuery->setFieldQuery('email', $search->getStringSearch());
+            $fieldQuery->setFieldQuery('email', $search->getStringSearch());
+            $fieldQuery->setFieldFuzziness('email', 0.7);
+            $query_part_user->addShould($fieldQuery);
+            $fieldQuery2->setFieldQuery('username', $search->getStringSearch());
+            $fieldQuery2->setFieldQuery('username', $search->getStringSearch());
+            $fieldQuery2->setFieldFuzziness('username', 0.7);
+            $query_part_user->addShould($fieldQuery2);
+            $filters = new \Elastica\Filter\Bool();
+            $query_user = new \Elastica\Query\Filtered($query_part_user, $filters);
+            $result_user = $typeUser->search($query_user);
 
         //Recherche Machine
-        $typeMachine = $this->get('fos_elastica.index.fablab.Machine');
-        $query_part_machine = new \Elastica\Query\Bool();
+            $typeMachine = $this->get('fos_elastica.index.fablab.Machine');
+            $query_part_machine = new \Elastica\Query\Bool();
 
-        $fieldQuery3 = new \Elastica\Query\Match();
-        $fieldQuery4 = new \Elastica\Query\Match();
-        $fieldQuery3->setFieldQuery('machine_name', $search->getStringSearch());
-        $fieldQuery3->setFieldFuzziness('machine_name', 0.7);
-        $fieldQuery3->setFieldMinimumShouldMatch('machine_name', '80%');
-        $query_part_machine->addShould($fieldQuery3);
-        $fieldQuery4->setFieldQuery('description', $search->getStringSearch());
-        $fieldQuery4->setFieldFuzziness('description', 2);
-        $fieldQuery4->setFieldMinimumShouldMatch('description', '100%');
-        $query_part_machine->addShould($fieldQuery4);
+            $fieldQuery3 = new \Elastica\Query\Match();
+            $fieldQuery4 = new \Elastica\Query\Match();
+            $fieldQuery3->setFieldQuery('machine_name', $search->getStringSearch());
+            $fieldQuery3->setFieldFuzziness('machine_name', 0.7);
+            $fieldQuery3->setFieldMinimumShouldMatch('machine_name', '80%');
+            $query_part_machine->addShould($fieldQuery3);
+            $fieldQuery4->setFieldQuery('description', $search->getStringSearch());
+            $fieldQuery4->setFieldFuzziness('description', 2);
+            $fieldQuery4->setFieldMinimumShouldMatch('description', '100%');
+            $query_part_machine->addShould($fieldQuery4);
        
-           $filters = new \Elastica\Filter\Bool();
-           $query_machine = new \Elastica\Query\Filtered($query_part_machine, $filters);
-           $result_machine = $typeMachine->search($query_machine);
+            $filters = new \Elastica\Filter\Bool();
+            $query_machine = new \Elastica\Query\Filtered($query_part_machine, $filters);
+            $result_machine = $typeMachine->search($query_machine);
 
 
 
@@ -102,19 +100,19 @@ class SearchStringController extends Controller
 
 
  //Recherche Machine
-        $typeMachine = $this->get('fos_elastica.index.fablab.Machine');
-        $query_part_machine = new \Elastica\Query\Bool();
+            $typeMachine = $this->get('fos_elastica.index.fablab.Machine');
+            $query_part_machine = new \Elastica\Query\Bool();
 
-        $fieldQuery3 = new \Elastica\Query\Match();
-        $fieldQuery4 = new \Elastica\Query\Match();
-        $fieldQuery3->setFieldQuery('machine_name', $search->getStringSearch());
+            $fieldQuery3 = new \Elastica\Query\Match();
+            $fieldQuery4 = new \Elastica\Query\Match();
+            $fieldQuery3->setFieldQuery('machine_name', $search->getStringSearch());
        // $fieldQuery3->setFieldFuzziness('machine.machine_name', 0.7);
         //$fieldQuery3->setFieldMinimumShouldMatch('machine_name', '80%');
-        $query_part_machine->addShould($fieldQuery3);
-        $fieldQuery4->setFieldQuery('description', $search->getStringSearch());
+            $query_part_machine->addShould($fieldQuery3);
+            $fieldQuery4->setFieldQuery('description', $search->getStringSearch());
        // $fieldQuery4->setFieldFuzziness('machine.machine_name', 0.7);
        //$fieldQuery4->setFieldMinimumShouldMatch('machine_name', '100%');
-        $query_part_machine->addShould($fieldQuery4);
+            $query_part_machine->addShould($fieldQuery4);
 
 
 
