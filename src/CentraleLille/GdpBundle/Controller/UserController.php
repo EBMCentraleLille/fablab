31d08f657aab6d3e832b736bf10dcf5f26c18c27
YassineKhlayfia <yassine.khlayfia@gmail.com>
2016-03-14 12:11:16 +0100

    corrected push problems

--- src/CentraleLille/GdpBundle/Controller/UserController.php
+++ src/CentraleLille/GdpBundle/Controller/UserController.php
@@ -9,7 +9,67 @@
 namespace CentraleLille\GdpBundle\Controller;
 
 
-class UserController
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use CentraleLille\CustomFosUserBundle\Entity\Project;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+class UserController extends FOSRestController
 {
+    /**
+     * Return the project members.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "Return the project members",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when no users are found"
+     *   }
+     * )
+     *
+     * @param int $id id
+     *
+     * @return View
+     */
+    public function getProjectUsersAction($id)
+    {
+        $projectRepository = $this->getDoctrine()->getRepository('CentraleLilleCustomFosUserBundle:Project');
+        $users = $projectRepository->find($id)->getProjectUsers();
+        if (!$users) {
+            throw $this->createNotFoundException('Data not found.');
+        }
+        $view = View::create();
+        $view->setData($users)->setStatusCode(200);
+        return $view;
+    }
+
+    /**
+     * Return the project .
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "Return the project",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when no users are found"
+     *   }
+     * )
+     *
+     * @param int $id id
+     *
+     * @return View
+     */
+    public function getProjectAction($id)
+    {
+        $projectRepository = $this->getDoctrine()->getRepository('CentraleLilleCustomFosUserBundle:Project');
+        $project = $projectRepository->find($id);
+        if (!$project) {
+            throw $this->createNotFoundException('Data not found.');
+        }
+        $view = View::create();
+        $view->setData($project)->setStatusCode(200);
+        return $view;
+    }
 
 }
