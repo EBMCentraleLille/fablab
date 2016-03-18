95b250b541fcb9ad4e62a41a3a2e787fed35b00c
pierloui <plbonnart@gmail.com>
2016-03-18 14:02:19 +0100

    Adding forgotten calendar.js file (with modified path)

---
+++ web/js/calendar-settings.js
@@ -0,0 +1,51 @@
+$(function () {
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+    $('#calendar-holder').fullCalendar({
+        header: {
+            left: 'prev, next',
+            center: 'title',
+            right: 'month, agendaWeek, agendaDay,'
+        },
+        lazyFetching: true,
+        timeFormat: {
+            // for agendaWeek and agendaDay
+            agenda: 'h:mmt',    // 5:00 - 6:30
+
+            // for all other views
+            '': 'h:mmt'         // 7p
+        },
+        eventSources: [
+            {
+                url: Routing.generate('fullcalendar_loader'),
+                type: 'POST',
+                // A way to add custom filters to your event listeners
+                data: {
+                    machineId: window.location.pathname.split('/')[window.location.pathname.split('/').length-1]
+                },
+                error: function() {
+                   alert('Erreur de récupération du calendrier, merci de recharger la page');
+                }
+            }
+        ],
+        selectable: true,
+        selectHelper: true,
+        select: function(start, end) {
+            var title = prompt('Event Title:');
+            var eventData;
+            if (title) {
+                eventData = {
+                    title: title,
+                    start: start,
+                    end: end
+                };
+                $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+            }
+            $('#calendar').fullCalendar('unselect');
+        },
+        defaultView: 'agendaWeek'
+    });
+});
