4a9a13f8f0f1bc3521ae1b5536d2f0ff67249e75
JamesHyot <james.hyot@gmail.com>
2016-03-25 18:08:08 +0100

    ProjectPage v0.2

--- src/CentraleLille/ProjectPageBundle/Controller/ProjectPageController.php
+++ src/CentraleLille/ProjectPageBundle/Controller/ProjectPageController.php
@@ -2,7 +2,7 @@
 /**
  * ProjectPageController.php File Doc
  *
- * Controller permettant l'affichage de la page d'un projet sur la route /projet/{projectId}
+ * Controller permettant l'affichage de la page d'un projet sur la route /project/{projectId}
  *
  * PHP Version 5.5
  *
@@ -15,7 +15,12 @@
 
 namespace CentraleLille\ProjectPageBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use CentraleLille\NewsFeedBundle\Entity\Activity;
+use CentraleLille\ProjectPageBundle\Form\ActivityType;
+use CentraleLille\CustomFosUserBundle\Entity\User;
+use CentraleLille\CustomFosUserBundle\Entity\Project;
 
 /**
 
@@ -32,35 +37,80 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProjectPageController extends Controller
 {
-    public function displayProjectAction($projectId)
+    /**
+     * displayProjectAction
+     *
+     * Affiche une page projet en utilisant l'ID du projet et en supposant récupérer les données du
+     * projet grâce à un service
+     *
+     * @param String $projectId Id unique de projet, attribué à la création par le groupe Projet
+     *        Request $request Requête http
+     *
+     * @return Response Une réponse à afficher
+     */
+    public function displayProjectAction($projectId, Request $request)
     {
-        /**
-         * displayProjectAction
-         *
-         * Affiche une page projet en utilisant l'ID du projet et en supposant récupérer les données du
-         * projet grâce à un service
-         *
-         * @param String $projectId Id unique de projet, attribué à la création par le groupe Projet
-         *
-         * @return Response Une réponse à afficher
-         */
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $project = $em
+            ->getRepository('CustomFosUserBundle:Project')
+            ->findOneBy(array('id'=>$projectId));
 
-        return $this->render('ProjectPageBundle:Default:projectpage.html.twig', array(
-                'projectId' => $projectId));
-    }
+        //Récupération des activités
+        $activityService=$this->container->get('fablab_newsfeed.activities');
+        $activities=$activityService->getActivityProjet($project, 3);     
 
-    public function wrongProjectAction()
-    {
-        /**
-         * wrongProjectAction
-         *
-         * Affiche une page d'erreur si l'on tente d'accéder à l'URL /project/ sans Id projet
-         *
-         * @param
-         *
-         * @return Response Une réponse à afficher
-         */
+        if($user){
+            if ($user->hasProject($project->getName())) {
+                
+                //affichage du formulaire et gestion de la requête
+                $activity = new Activity();
+                $form = $this->createForm(ActivityType::class, $activity);
+                
+                $form->handleRequest($request);
 
-        return $this->render('ProjectPageBundle:Default:wrongpage.html.twig');
+                if ($form->isSubmitted() && $form->isValid()) {
+                    $activity->setDate(new \Datetime());
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($activity);
+                    $em->flush();
+                    $session=$request->getSession()->getFlashBag()->add(
+                        'notice',
+                        "L'activité a bien été ajoutée."
+                    );
+                    
+                    return $this->redirect(
+                        $this->generateUrl(
+                        )
+                    );
+                    
+                }
+                
+            return $this->render(
+                'ProjectPageBundle:Default:projectpage.html.twig',
+                array(
+                    'project' => $project,
+                    'recentActivities' => $activities,
+                    'form' => $form->createView()
+                )
+            );
+            }
+            return $this->render(
+                'ProjectPageBundle:Default:projectpage.html.twig',
+                array(
+                    'project' => $project,
+                    'recentActivities' => $activities
+                )
+            );
+        }
+        else{
+            return $this->render(
+                'ProjectPageBundle:Default:projectpage.html.twig', 
+                array(
+                    'project' => $project,
+                    'recentActivities' => $activities,
+                    )
+                );
+        }
     }
 }
