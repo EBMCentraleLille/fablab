8477b93c8134698d7982c3da80b9b28b3f8b3dd6
matthieu <ml.94230@gmail.com>
2016-03-29 23:30:27 +0200

    Task status

---
+++ src/CentraleLille/GdpBundle/Enum/TaskStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CentraleLille\GdpBundle\Enum;
+
+abstract class TaskStatus extends BasicEnum {
+    const PLANIFIE = "PLANIFIE";
+    const EN_COURS = "EN_COURS";
+    const TERMINE = "TERMINE";
+    const ARCHIVE = "ARCHIVE";
+}
