0362694016300ba7e7ea0c5be219b1418c5d8d0b
Achille <achille.urbain@gmail.com>
2016-03-30 08:33:16 +0200

    Fix tests

--- src/CentraleLille/CustomFosUserBundle/Tests/Controller/UserControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CentraleLille\CustomFosUserBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class UserControllerTest extends WebTestCase
-{
-}
--- src/CentraleLille/KnowledgeBundle/Tests/Controller/UserCompetenceControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CentraleLille\KnowledgeBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class UserCompetenceControllerTest extends WebTestCase
-{
-}
